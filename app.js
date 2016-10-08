63f3f06900816179c81c1bf72f37b41a452110a1
daijinload <daijinload@gmail.com>
2016-10-09 04:31:17 +0900

    aaaa

--- app.js
+++ app.js
@@ -7,27 +7,7 @@ onload = function () {
     };
 
     var webview = document.getElementById("foo");
-    var indicator = document.querySelector(".indicator");
 
-    var loadstart = function () {
-        indicator.innerText = "loading...";
-    };
-
-    var loadstop = function () {
-        indicator.innerText = "";
-    };
-
-    webview.addEventListener("did-start-loading", loadstart);
-    webview.addEventListener("did-stop-loading", loadstop);
-
-    var btnBack = document.getElementById("btnBack");
-    btnBack.onclick = function () {
-        webview.goBack();
-    };
-    var btnFwd = document.getElementById("btnFwd");
-    btnFwd.onclick = function () {
-        webview.goForward();
-    };
     var btnPrnt = document.getElementById("btnPrnt");
     btnPrnt.onclick = function () {
         webview.printToPDF({}, function (err, data) {
